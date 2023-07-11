33872b87fcf39ed666fdba061a312ee2ab91e91a
Kev COLIN <colin-kevin@outlook.fr>
2023-07-11 14:17:49 +0200

    first commit

---
+++ backend/controllers/books.js
@@ -0,0 +1,120 @@
+const Book = require('../models/Book');
+const fs = require('fs');
+
+exports.createBook = (req, res, next) => {
+    const bookObject = JSON.parse(req.body.book);
+    delete bookObject._id;
+    delete bookObject._userId;
+    const book = new Book({
+        ...bookObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        averageRating: bookObject.ratings[0].rating
+    });
+  
+    book.save()
+    .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+    .catch(error => { res.status(400).json( { error })})
+ };
+
+ exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
+
+ exports.deleteBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id})
+        .then(book => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({message: 'Not authorized'});
+            } else {
+                const filename = book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+ };
+
+  exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+  };
+  
+  exports.getAllBook = (req, res, next) => {
+    Book.find()
+   .then(books => res.status(200).json(books))
+   .catch(error => res.status(404).json({ error }));
+  };
+
+exports.postBookRating = (req, res, next) => {
+
+    const newRating = { ...req.body };
+  delete newRating.rating;
+
+    Book.findOne({ _id: req.params.id })
+  .then(book => {
+const savBook ={...book._doc};
+savBook.rating = [{ ...newRating}, ...Book.rating];
+
+function calcAverageRating(arr) {
+    let avr = Math.round((arr.reduce((acc, elem) => acc + elem.grade, 0) / arr.length) * 100) / 100;
+    return avr;
+  };
+  savBook.averageRating = calcAverageRating(savBook.ratings);
+
+  Book.updateOne(
+    { _id: req.params.id },
+    {...savBook}
+    )
+    .then(() => {
+      res.status(200).json(savBook);
+    })
+    .catch((error) => {
+      res.status(401).json({ error });
+    });
+})
+.catch((error) => {
+  res.status(400).json({ error });
+});
+};
+
+exports.getBestBooks = (req, res, next) => {
+    Book.find()
+      .then((books) => {
+        res
+          .status(200)
+          // Pour récupérer les meilleures livres, je copie mon tableau de datas;
+          // je les classe de façon décroissante avec sort, et splice me permet de
+          //récupérer les 3 premiers livres.
+          .json(
+            [...books]
+              .sort((a, b) => b.averageRating - a.averageRating)
+              .splice(0, 3)
+          );
+      })
+      .catch((error) => res.status(400).json({ error }));
+  };
---
+++ backend/routes/books.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const booksCtrl = require('../controllers/books');
+
+router.get('/', booksCtrl.getAllBook);
+router.post('/', auth, multer, booksCtrl.createBook);
+router.get('/:id', booksCtrl.getOneBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.post('/:id/rating', auth, booksCtrl.postBookRating);
+router.get("/bestrating", booksCtrl.getBestBooks);
+
+module.exports = router;
